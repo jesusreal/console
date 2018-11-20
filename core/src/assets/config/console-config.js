c59a648a25c340453fb4bb2ee8fe21a36fb9db0f
y-kkamil <>
2018-11-20 13:36:04 +0100

    fix bug in service instances

--- core/src/assets/config/console-config.js
+++ core/src/assets/config/console-config.js
@@ -62,7 +62,7 @@ function getNodes(environment) {
           children: [
             {
               pathSegment: ':name',
-              viewUrl: `${config.serviceCatalogModuleUrl}/details/:name`
+              viewUrl: `${config.serviceInstancesModuleUrl}/details/:name`
             }
           ]
         }
