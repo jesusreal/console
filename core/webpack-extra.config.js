36bb1e9d4e6b513a7377e7248e138c289b19832c
Kamil K <kamil.kupczynski@sap.com>
2018-12-18 10:46:07 +0100

    Update webpack-extra.config.js

--- core/webpack-extra.config.js
+++ core/webpack-extra.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   devServer: {
-    // because of 404 issue, which redirected us to sampleapp.html instead of index.html
     historyApiFallback: {
       rewrites: [{ from: /./, to: './src/index.html' }]
     }
