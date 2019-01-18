1e6b09ec65d5be9c43fade4bf4de9c6c56e1e73d
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-01-18 15:53:59 +0100

    Redesign of environment create component

--- core/src/app/content/environments/environment-create/environment-create.component.ts
+++ core/src/app/content/environments/environment-create/environment-create.component.ts
@@ -1,6 +1,8 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import  {ViewChild} from '@angular/core';
 import { EnvironmentsService } from '../services/environments.service';
 import LuigiClient from '@kyma-project/luigi-client';
+import { ModalService } from 'fundamental-ngx';
 
 @Component({
   selector: 'app-environment-create',
@@ -10,13 +12,19 @@ import LuigiClient from '@kyma-project/luigi-client';
 export class EnvironmentCreateComponent {
   @Output() cancelEvent: EventEmitter<any> = new EventEmitter();
 
+  @ViewChild('createEnvironmentModal') createEnvironmentModal: any;
+
   public environments = [];
   public environmentName: string;
   public isActive: boolean;
   private err: string;
   private wrongName = false;
 
-  constructor(private environmentsService: EnvironmentsService) {}
+  constructor(
+    private environmentsService: EnvironmentsService,
+    private modalService: ModalService
+    
+  ) {}
 
   public createEnvironment() {
     this.environmentsService.createEnvironment(this.environmentName).subscribe(
@@ -32,15 +40,20 @@ export class EnvironmentCreateComponent {
   }
 
   public cancel() {
-    this.isActive = false;
-    this.wrongName = false;
-    this.cancelEvent.emit();
+    this.modalService.close(this.createEnvironmentModal);
   }
 
   public show() {
     this.environmentName = '';
     this.err = undefined;
     this.isActive = true;
+
+    this.modalService.open(this.createEnvironmentModal).result
+        .finally(() => {
+          this.isActive = false;
+          this.wrongName = false;
+          this.cancelEvent.emit();      
+        })
   }
 
   public navigateToDetails(envName) {
