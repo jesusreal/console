0fbf79e867a1b242286696d856016b2717d5d9b3
Maryna Khromova <46446373+marynaKhromova@users.noreply.github.com>
2019-01-29 17:11:30 +0100

    replace buttons with links in tables, rework json editor modal component

--- core/src/app/shared/components/json-editor-modal/json-editor-modal.component.ts
+++ core/src/app/shared/components/json-editor-modal/json-editor-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { JsonEditorComponent } from './json-editor/json-editor.component';
 import { ComponentCommunicationService } from '../../services/component-communication.service';
+import { ModalService } from 'fundamental-ngx';
 
 @Component({
   selector: 'app-json-editor-modal',
@@ -10,19 +11,29 @@ import { ComponentCommunicationService } from '../../services/component-communic
 export class JsonEditorModalComponent {
   @Input() resourceData: any;
   @ViewChild('jsoneditor') jsonEditor: JsonEditorComponent;
+  @ViewChild('jsonEditorModal') jsonEditorModal: any;
+
   public isActive = false;
   public error: any;
 
-  constructor(private communicationService: ComponentCommunicationService) {}
+  constructor(
+    private communicationService: ComponentCommunicationService,
+    private modalService: ModalService
+  ) {}
 
   show() {
     this.isActive = true;
+
+    this.modalService.open(this.jsonEditorModal).result
+        .finally(() => {
+          this.isActive = false;
+          this.error = false;
+          event.stopPropagation();    
+        })
   }
 
   cancel(event: Event) {
-    this.isActive = false;
-    this.error = false;
-    event.stopPropagation();
+    this.modalService.close(this.jsonEditorModal);
   }
 
   update(event: Event) {
@@ -34,6 +45,7 @@ export class JsonEditorModalComponent {
           type: 'updateResource',
           data
         });
+        this.modalService.close(this.jsonEditorModal);
       },
       err => this.displayErrorMessage(err)
     );
@@ -43,3 +55,56 @@ export class JsonEditorModalComponent {
     this.error = httpErrorResponse.error;
   }
 }
+
+
+
+
+
+
+
+// import { Component, Input, ViewChild } from '@angular/core';
+// import { JsonEditorComponent } from './json-editor/json-editor.component';
+// import { ComponentCommunicationService } from '../../services/component-communication.service';
+
+// @Component({
+//   selector: 'app-json-editor-modal',
+//   templateUrl: './json-editor-modal.component.html',
+//   styleUrls: ['./json-editor-modal.component.scss']
+// })
+// export class JsonEditorModalComponent {
+//   @Input() resourceData: any;
+//   @ViewChild('jsoneditor') jsonEditor: JsonEditorComponent;
+//   public isActive = false;
+//   public error: any;
+
+//   constructor(private communicationService: ComponentCommunicationService) {}
+
+//   show() {
+//     this.isActive = true;
+//   }
+
+//   cancel(event: Event) {
+//     this.isActive = false;
+//     this.error = false;
+//     event.stopPropagation();
+//   }
+
+//   update(event: Event) {
+//     this.jsonEditor.updateYaml().subscribe(
+//       data => {
+//         event.stopPropagation();
+//         this.isActive = false;
+//         this.communicationService.sendEvent({
+//           type: 'updateResource',
+//           data
+//         });
+//       },
+//       err => this.displayErrorMessage(err)
+//     );
+//   }
+
+//   displayErrorMessage(httpErrorResponse) {
+//     this.error = httpErrorResponse.error;
+//   }
+// }
+
