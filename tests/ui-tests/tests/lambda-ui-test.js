9fb4d73a9e09fae9d27f81377957738b0d725733
Sudczak, Jan <jan.sudczak@sap.com>
2019-02-21 09:06:25 +0100

    Replace data-testid with data-e2e

--- tests/ui-tests/tests/lambda-ui-test.js
+++ tests/ui-tests/tests/lambda-ui-test.js
@@ -58,7 +58,7 @@ describeIf(dex.isStaticUser(), 'Lambda UI tests', () => {
 
     // given (go to create lambda)
     const frame = await kymaConsole.getFrame(page);
-    const lambdasEmptyPage = '[data-testid="empty-list-placeholder"]';
+    const lambdasEmptyPage = '[data-e2e="empty-list-placeholder"]';
     await frame.waitForSelector(lambdasEmptyPage);
     const currentLambdas = await lambdas.getLambdas(frame);
     const addLambdaButton = '.tn-button.tn-button--small.tn-button--text';
@@ -107,7 +107,7 @@ describeIf(dex.isStaticUser(), 'Lambda UI tests', () => {
       await page.reload({ waitUntil: 'networkidle0' });
 
       const frame2 = await kymaConsole.getFrame(page);
-      const lambdasEmptyPage = '[data-testid="empty-list-placeholder"]';
+      const lambdasEmptyPage = '[data-e2e="empty-list-placeholder"]';
       await frame2.waitForSelector(lambdasEmptyPage);
       const expectedLambdas = await lambdas.getLambdas(frame2);
 
